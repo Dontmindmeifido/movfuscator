11a8d207d0b285a394234405a4e881d6e1409977
Ruslan <ruslangaitur@gmail.com>
2025-12-12 09:05:06 +0200

    add lookup_add

--- movfuscator.py
+++ movfuscator.py
@@ -1,12 +1,10 @@
 # .data
-#    lookup_mul: .space 65536 0, 1, 2...
+#    lookup_add: .space 65536 0, 1, 2...
 
 
 # 8 bits
-lookup_mul = ""
+lookup_add = ""
 for op2 in range(256):
     for op1 in range(256):
         # At memory op1 + 255 * op2
-        lookup_mul = lookup_mul + f"{op1+op2} "
-
-print(lookup_mul)
+        lookup_add = lookup_add + f"{op1+op2} "

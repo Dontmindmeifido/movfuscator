e4ed6cf25a0697c19607f36b28fc3f8278351ef6
Ruslan <ruslangaitur@gmail.com>
2025-12-17 21:59:26 +0200

    add mul and progexec logic

--- movfuscator.py
+++ movfuscator.py
@@ -1,4 +1,4 @@
-#--------------- HELP ---------------#
+#--------------- HELP (OUTDATED) ---------------#
 # top1 - temporary operand 1
 # top2 - temporary operand 2
 # tax - temporary register eax
@@ -12,7 +12,7 @@
 # lookup_orr - lookup table for or (or is unavailable)
 # lookup_nott - lookup table for not (not is unavailable)
 # lookup_xor - lookup table for xor
-#--------------- HELP ---------------#
+#--------------- HELP (OUTDATED) ---------------#
 
 
 
@@ -92,6 +92,7 @@ def add(op1, op2):
 
     ret += restore_registers()
 
+    # Potentially add another add function that simply writes to eax --TODO--
     if op2.startswith("%"):
         ret += f"movl top2, {op2}\n"
     elif op2.startswith("$"):
@@ -100,6 +101,95 @@ def add(op1, op2):
     else:
         ret += f"movl top2, %eax\n"
         ret += f"movl %eax, {op2}\n" 
+
+    return ret
+
+def trlupdprogexec(op1, op2):
+    '''
+    Trailing update program execution
+    
+    :param op1: Variable
+    :param op2: Constant
+    '''
+    ret = ""
+
+    ret += save_registers()
+
+    # Initialize
+    ret += "movl $eqtrash, %eax\n"
+    ret += f"movl {op1}, %ebx\n"
+    ret += f"movl {op2}, %ecx\n"
+    ret += "movl progexec, %edx\n"
+
+    # Check equality
+    ret += "movl $0, %esi\n"
+    ret += "movl %esi, (%eax, %ebx, 4)\n"
+
+    ret += "movl $1, %esi\n"
+    ret += "movl %esi, (%eax, %ecx, 4)\n"
+
+    # Get result and do (result or progexec value)
+    ret += "movl (%eax, %ebx, 4), %eax\n"
+    ret += orr("%eax", "%edx\n")
+
+    # Update progexec with the latest state
+    ret += "movl %edx, progexec\n"
+
+    ret += restore_registers()
+
+    return ret
+
+#CONSTRUCTION
+
+def mul(op1, op2):
+    '''
+    ATTENTION: MUL SAVES RESULT IN EAX (as opposed to add which saves to destination operand)
+    
+    :param op1: operand 1
+    :param op2: operand 2
+    '''
+    ret = ""
+
+    ret += save_registers()
+
+    # Initialize branch
+    ret += "movl $branch, %edi"
+
+    ret += "movl $0, %eax"
+    ret += "movl $topm1, %ebx" # we will save the real branch at the adress of topm1 (Be careful, progexec initially starts at 0 so we want that)
+    ret += "movl %ebx, (%edi, %eax, 4)"
+
+    ret += "movl $1, %eax"
+    ret += "movl $topm2, %ebx" # we will save the fake branch at the adress of topm2 (Be careful, progexec initially starts at 0 so we want that)
+    ret += "movl %ebx, (%edi, %eax, 4)"
+
+    # Initialize variables
+    ret += f"movl {op2}, %ebx"
+    ret += "movl $0, %ecx"
+    ret += f"movb {op1}, %cl"
+
+    ret += "movl $0, %edx"
+    ret += "movl %edx, topm1"
+
+
+    for i in range(256):
+        # Do the addition %edx with op2
+        ret += add(f"{op2}", "%edx")
+
+        # Update progexec (%ecx is op1)
+        ret += trlupdprogexec("%ecx", f"{i}")
+
+        # Save the result to branch (if we wrote to it good, if not then also good)
+        ret += "movl progexec, %eax"
+        ret += "movl (%edi, %eax, 4), %esi"
+        ret += "movl %edx, (%esi)" 
+
+        ret += "movl topm1, %edx"
+    
+    ret += restore_registers()
+
+    # Final result will be in eax
+    ret += "movl topm1, %eax"
 
     return ret
 
@@ -258,8 +348,8 @@ def xor(op1, op2):
 
     return ret
 
-out = open("out.s", "w")
 
+out = open("out.s", "w")
 with open("in.s", "r") as assembly:
     for line in assembly.readlines():
         line = line.strip()
@@ -285,6 +375,12 @@ with open("in.s", "r") as assembly:
             
             out.write("    top1: .space 4 \n")
             out.write("    top2: .space 4 \n")
+            out.write("    topm1: .space 4 \n")
+            out.write("    topm2: .space 4 \n")
+
+            out.write("    progexec: .space 4 \n") # dictates program flow
+            out.write("    branch: .space 8 \n") # 4 bytes of memory at ($branch, $0, 4) and 4 at ($branch, $1, 4) [obv, arg1 and arg2 in registers]
+            out.write("    eqtrash: .space 1024 \n") # trash memory to be used for equality checks between LONG and BYTE at most
         elif (line.split()[0] == ".main"):
             out.write(line + "\n")
             # For future use to generate lookup tables ok
@@ -299,6 +395,12 @@ with open("in.s", "r") as assembly:
                 op1 = args[1]
                 op2 = args[2]
                 out.write(add(op1, op2))
+            elif line.split()[0] == "mul":
+                line = line.replace(",", " ")
+                args= line.split()
+                op1 = args[1]
+                op2 = args[2]
+                out.write(mul(op1, op2))
             elif line.split()[0] == "and":
                 line = line.replace(",", " ")
                 args= line.split()
